69aa3c6cdb6ee76d58f7b93ab2cabad625ced601
MariaBelyakovaa <mariya.belyakova.06@bk.ru>
2025-12-21 14:46:41 +0300

    Создаем view для главной страницы

--- events/views.py
+++ events/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
+from .models import Event, Participation, Comment
+from django.contrib import messages
 
-# Create your views here.
+def index(request):
+    events = Event.objects.filter(date__gte=timezone.now()).order_by('date')
+    
+    
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        date = request.POST.get('date')
+        location = request.POST.get('location')
+        category = request.POST.get('category')
+        description = request.POST.get('description')
+        
+        if title and date and location:
+            
+            event = Event.objects.create(
+                title=title,
+                date=date,
+                location=location,
+                category=category,
+                description=description
+            )
+            messages.success(request, f'Событие "{title}" успешно добавлено!')
+            return redirect('index')
+        else:
+            messages.error(request, 'Заполните все обязательные поля!')
+    
+    
+    events_count = Event.objects.count()
+    nearest_event = events.first().title if events.exists() else "Нет событий"
+    
+    context = {
+        'events': events[:12],  
+        'events_count': events_count,
+        'nearest_event': nearest_event,
+    }
+    
+    return render(request, 'events/index.html', context)
+
+def event_detail(request, event_id):
+    
+    event = get_object_or_404(Event, id=event_id)
+    return render(request, 'events/detail.html', {'event': event})
